4735b6e128c375d9fae392a14ddb290c17bd9509
dnddma444 <158276631+dnddma444@users.noreply.github.com>
2024-03-27 20:13:37 +0900

    Title: 높은 수에서 낮은 수까지, Time: 112ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240327/높은 수에서 낮은 수까지/from-high-to-low.py
@@ -0,0 +1,12 @@
+arr=input().split()
+
+a=int(arr[0])
+b=int(arr[1])
+
+if a>b:
+    for i in range(b,a+1,-1):
+        print (i,end=' ')
+
+elif b>a:
+    for i in range (b,a-1,-1):
+        print(i,end=' ')

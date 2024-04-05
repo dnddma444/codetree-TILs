eadf4e5471ca6ba15aba8b26b53c8bb59392d404
dnddma444 <158276631+dnddma444@users.noreply.github.com>
2024-04-05 11:03:01 +0900

    Title: 배수의 개수, Time: 145ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240405/배수의 개수/number-of-multipliers.py
@@ -0,0 +1,14 @@
+cnt3=0
+cnt5=0
+
+for _ in range(10):
+    num = int(input())
+    if num%3==0 and num%5==0:
+        cnt3+=1
+        cnt5+=1
+    elif num%3==0:
+        cnt3+=1
+    elif num%5==0:
+        cnt5+=1
+        
+print(cnt3,cnt5)

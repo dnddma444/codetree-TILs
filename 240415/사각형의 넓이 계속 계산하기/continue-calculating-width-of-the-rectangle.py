9889ced63db6017cca24623b619b7529a87ef793
dnddma444 <158276631+dnddma444@users.noreply.github.com>
2024-04-15 11:43:44 +0900

    Title: 사각형의 넓이 계속 계산하기, Time: 122ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240415/사각형의 넓이 계속 계산하기/continue-calculating-width-of-the-rectangle.py
@@ -0,0 +1,12 @@
+while True:
+    arr=input().split()
+    ga=int(arr[0])
+    se=int(arr[1])
+    mun=arr[2]
+    if mun=='C':
+        print(ga*se)
+        break
+    
+    else:
+        print(ga*se)
+        continue

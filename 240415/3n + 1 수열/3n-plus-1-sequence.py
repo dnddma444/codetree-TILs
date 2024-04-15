4df34744a15385a53561519ca1ec9f20a2ee4aea
dnddma444 <158276631+dnddma444@users.noreply.github.com>
2024-04-15 11:59:53 +0900

    Title: 3n + 1 수열, Time: 261ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240415/3n + 1 수열/3n-plus-1-sequence.py
@@ -0,0 +1,15 @@
+n=int(input())
+cnt=0
+while True:
+    if n%2==0 :
+        n/=2
+        cnt+=1
+
+    elif n%2==1 and n!=1:
+        n*=3
+        n+=1
+        cnt+=1
+    
+    elif n==1:
+        print(cnt)
+        break

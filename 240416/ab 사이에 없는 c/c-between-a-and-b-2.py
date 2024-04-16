dc56d6784d51ce9c219a0ef4898ad4dae89a5e4c
dnddma444 <158276631+dnddma444@users.noreply.github.com>
2024-04-16 10:21:59 +0900

    Title: ab 사이에 없는 c, Time: 114ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240416/ab 사이에 없는 c/c-between-a-and-b-2.py
@@ -0,0 +1,15 @@
+arr=input().split()
+a,b,c=int(arr[0]),int(arr[1]),int(arr[2])
+sat=True
+
+for i in range (a,b+1):
+    if i%c!=0:
+        sat=True
+    elif i%3==0:
+        sat=False
+        break
+
+if sat==True:
+    print('YES')
+else:
+    print('NO')
